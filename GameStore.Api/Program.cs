0c08c2588706834d17020f5833219cfcd5a9b63f
Ravindu Malinga <ravindu_d@epiclanka.net>
2024-05-02 09:15:59 +0530

    post endpoint done

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,12 @@
+using GameStore.Api;
 using GameStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
+
+const string GetGameEndpointName = "GetGame";
 
 List<GameDto> games = [
     new (
@@ -34,7 +37,28 @@ List<GameDto> games = [
 app.MapGet("games",()=>games);
 
 // Get game by id
-app.MapGet("games/{id}",(int id)=>games.Find(game=>game.Id==id));
+app.MapGet("games/{id}",(int id)=>games.Find(game=>game.Id==id))
+.WithName(GetGameEndpointName);
+
+
+// POST /games
+app.MapPost("games",(CreateGameDto newGame)=>
+{
+    GameDto game = new(
+        games.Count+1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+
+        
+    );
+
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new{id = game.Id},game);
+  
+});
+
 
 app.MapGet("/", () => "Hello World!");
 
